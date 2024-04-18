9d94bce0eddfe153d4ed75a2f1c29e0d41c774c0
Amish Toor <95700722+Amish360@users.noreply.github.com>
2024-04-18 21:35:29 +0500

    Continued working on events page

--- src/components/allevents.tsx
+++ src/components/allevents.tsx
@@ -1,3 +1,52 @@
-export default function AllEvents(){
-    return<h1>All Events</h1>
-};
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from './styles.module.css';
+
+const eventData = [
+  {
+    title: 'Cloud Seekho Workshop Series',
+    type: 'Workshop',
+    imageUrl: 'https://via.placeholder.com/200x200',
+  },
+  {
+    title: 'Figma Design Competition',
+    type: 'Competition',
+    imageUrl: 'https://via.placeholder.com/200x200',
+  },
+  {
+    title: 'Machine Learning:Getting into AI',
+    type: 'Awareness Session',
+    imageUrl: 'https://via.placeholder.com/200x200',
+  },
+  {
+    title: 'Angular Workshop',
+    type: 'Workshop',
+    imageUrl: 'https://via.placeholder.com/200x200',
+  },
+  {
+    title: 'Tech Baithak',
+    type: 'Awareness Session',
+    imageUrl: 'https://via.placeholder.com/200x200',
+  },
+  {
+    title: 'Personal Brand Content',
+    type: 'Awareness Session',
+    imageUrl: 'https://via.placeholder.com/200x200',
+  },
+];
+
+export default function AllEvents() {
+  return (
+    <div className="row">
+        {eventData.map(event => (
+            <div key={event.id} className="col-lg-4">
+            <div className={styles.card-container}>
+                <img className={styles.image} src={event.imageUrl} alt="Event" />
+                <div className={styles.name}>{event.title}</div>
+                <div className={styles.eventType}>{event.type}</div>
+            </div>
+            </div>
+        ))}
+    </div>
+  );
+}
--- src/components/speakers.tsx
+++ src/components/speakers.tsx
@@ -1,3 +1,71 @@
-export default function Speakers(){
-    return<h1>Speakers</h1>
-};
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from '../styles/eventsandspeakers.module.css';
+
+const speakersData = [
+  {
+    name: "Michelle Curry",
+    position: "CEO at TOQQA Global",
+    event: "Speaker at",
+    eventTitle: "TECH TALK: THE POWER OF WOMEN ENTREPRENEURSHIP",
+    linkedinProfile: "Linked In Profile URL"
+  },
+  {
+    name: "Michelle Curry",
+    position: "CEO at TOQQA Global",
+    event: "Speaker at",
+    eventTitle: "TECH TALK: THE POWER OF WOMEN ENTREPRENEURSHIP",
+    linkedinProfile: "Linked In Profile URL"
+  },
+  {
+    name: "Ahmed Ayub",
+    position: "Co-founder at Airlift Technologies",
+    event: "Speaker at",
+    eventTitle: "TECH TALK: HOW TO CHOOSE CAREER",
+    linkedinProfile: "Linked In Profile URL"
+  },
+  {
+    name: "Saad Hamid",
+    position: "Growth Manager @Google",
+    event: "Speaker at",
+    eventTitle: "TECH TALK: HOW TO THINK 10X IDEAS",
+    linkedinProfile: "Linked In Profile URL"
+  },
+  {
+    name: "Umer Abdullah",
+    position: "Software Engineer at Google",
+    event: "Speaker at",
+    eventTitle: "TECH TALK: GRAND LAUNCH EVENT OF GDSC AT DSU",
+    linkedinProfile: "Linked In Profile URL"
+  },
+  {
+    name: "Hisham Sarwar",
+    position: "Founder at Being Guru",
+    event: "Speaker at",
+    eventTitle: "TECH TALK: GET SET FREELANCE",
+    linkedinProfile: "Linked In Profile URL"
+  },
+];
+
+export default function Speakers() {
+  return (
+    <div className="row">
+        {speakersData.map(speaker => (
+            <div key={speaker.id} className="col-lg-6">
+            <div className={styles.cardContainer}>
+                <img className={styles.profileImage} src="https://via.placeholder.com/200x200" alt="Profile" />
+                <div className={styles.name}>{speaker.name}</div>
+                <div className={styles.position}>{speaker.position}</div>
+                <div className={styles.event}>{speaker.event}</div>
+                <div className={styles.eventTitle}>{speaker.eventTitle}</div>
+                <div className={styles.group718}>
+                <div className={styles.rectangle191}></div>
+                <div className={styles.vector}></div>
+                <div className={styles.linkedinProfile}>{speaker.linkedinProfile}</div>
+                </div>
+            </div>
+            </div>
+        ))}
+    </div>
+  );
+}
