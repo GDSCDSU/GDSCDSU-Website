5f75e500ec869f0e9dbb68e8853221273588b06c
Amish360 <amishtoor@gmail.com>
2024-06-18 21:35:03 +0500

    leads page made(a little bit of responsiveness issue remains))

---
+++ src/app/events/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+import React from 'react';
+import HomePage from '../page';
+
+
+const Events: React.FC = () => {
+  
+  return (
+    <main className="min-h-screen bg-white">
+        <HomePage/>
+    </main>
+  );
+};
+
+export default Events;
