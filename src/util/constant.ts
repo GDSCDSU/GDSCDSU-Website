4f164d7719d2333646dec2d16ae4116cdbb4a289
Amish360 <amishtoor@gmail.com>
2024-06-26 20:36:49 +0500

    Update constant.ts

--- src/util/constant.ts
+++ src/util/constant.ts
@@ -1,6 +1,6 @@
 import dynamic from 'next/dynamic';
 
-export const BASE_URL = "https://gdsc.dsu.edu.pk//api";
+export const BASE_URL = "https://gdscdsu.com/api";
 
 export const components = {
   Founder: dynamic(() => import('../components/founder'), { ssr: false }),
